d402f91c364ffcbaccb31e1020f19c2d01d22113
Mandu <tjdals6695@gmail.com>
2019-11-03 15:49:22 +0900

    Algorithm

--- Algorithm/bin.cpp
+++ Algorithm/bin.cpp
@@ -9,28 +9,38 @@ int home[501];
 int distanceSum[501][501];
 int cache[501][501];
 
+void init() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+			cache[i][j] = INFINITY;
+		}
+	}
+	memset(distanceSum, 0, sizeof(distanceSum));
+}
+
 int main() {
 	ifstream inp("bin.inp");
 	ofstream out("bin.out");
 	int testcase;
 	inp >> testcase;
 	while (testcase--) {
+		init();
 		inp >> n >> m;
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j <= m; j++) {
-				cache[i][j] = INFINITY;
-			}
-		}
-		memset(distanceSum, 0, sizeof(distanceSum));
 		for (int i = 1; i <= n; i++) {
 			inp >> home[i];
 		}
+
+		/*집의 위치대로 정렬*/
 		sort(home + 1, home + n + 1);
-		for (int i = 1; i <= n; i++) {
+
+		/*수집통을 하나만 썻을 때 최솟값*/
+		for (int i = 1; i <= n; i++) { 
 			for (int j = i; j <= n; j++) {
 				distanceSum[i][j] = distanceSum[i][j - 1] + home[j] - home[i + (j - i) / 2];
 			}
 		}
+
+		/*i개의 집을 j개의 수집통으로 배치했을 때 최솟값*/
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
 				for (int k = 1; k <= i; k++) {
