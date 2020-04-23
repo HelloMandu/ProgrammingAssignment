1d51bc05c3ec11fc4be9aefa734614a1200fb61e
Mandu <tjdals6695@gmail.com>
2020-04-23 18:47:30 +0900

    ProblemSolve

--- ProblemSolve/flow.cpp
+++ ProblemSolve/flow.cpp
@@ -9,19 +9,18 @@ using namespace std;
 
 struct Node {
 	int edge, capacity, flow;
-	Node *reverse;
 
 	int residualFlow() { // 잔여 유량
 		return capacity - flow;
 	}
 };
 
-vector<vector<Node*> > graph;
+vector<vector<Node> > graph;
 int parent[MAX];
 Node* route[MAX];
 int result = 0;
-bool canFlow(Node *node) { // 유량이 흐를 수 있는지 판단
-	return parent[node->edge] == -1 && node->residualFlow();
+bool canFlow(Node node) { // 유량이 흐를 수 있는지 판단
+	return parent[node.edge] == -1 && node.residualFlow();
 }
 
 bool networkFlow(int n) {
@@ -30,12 +29,12 @@ bool networkFlow(int n) {
 	while (!Queue.empty()) {
 		int node = Queue.front(); Queue.pop();
 		for (int i = 0; i < graph[node].size(); i++) {
-			Node *cur = graph[node][i];
+			Node &cur = graph[node][i];
 			if (canFlow(cur)) {
-				parent[cur->edge] = node;
-				route[cur->edge] = cur;
-				Queue.push(cur->edge);
-				if (cur->edge == n - 1) {
+				parent[cur.edge] = node;
+				route[cur.edge] = &cur;
+				Queue.push(cur.edge);
+				if (cur.edge == n - 1) {
 					return true;
 				}
 			}
@@ -53,7 +52,6 @@ void renualFlow(int n) {
 	cur = n - 1;
 	while (cur) {
 		route[cur]->flow += flow;
-		route[cur]->reverse->flow += flow;
 		cur = parent[cur];
 	}
 	result += flow;
@@ -76,8 +74,7 @@ int main() {
 			graph.clear(); inp.close(); out.close();
 			return 0;
 		}
-		Node *node1 = new Node({ j, w, 0, NULL }), *node2 = new Node({ i, w, 0, NULL });
-		node1->reverse = node2; node2->reverse = node1;
-		graph[i].push_back(node1); graph[j].push_back(node2);
+		graph[i].push_back({ j, w, 0 });
+		graph[j].push_back({ i, w, 0 });
 	}
 }
